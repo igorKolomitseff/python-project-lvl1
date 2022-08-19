b5f899e9134a0ef4705d26fdcb9b20a7c4771091
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-08-19 12:59:41 +0300

    Add import func. "welcome_user" to brain_games.py
    The function "welcome_user" is imported from brain_games/cli.py to
    brain_games/scripts/brain_games.py.

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+
 def show_greeting():
     print('Welcome to the Brain Games!')
 
 
 def main():
     show_greeting()
-
+    welcome_user()
 
 if __name__ == '__main__':
     main()
