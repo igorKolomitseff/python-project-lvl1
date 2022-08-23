512899c485bc4729ad630e3dd40742727ce93254
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-08-23 13:51:16 +0300

    Add func. "choose_random_number" to brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,12 @@
+from random import randint
+
+
+def choose_random_number():
+    """The function returns a integer number from a range
+    of numbers."""
+    begin_of_range = 1
+    end_of_range = 100
+
+    random_number = randint(begin_of_range, end_of_range)
+
+    return random_number
