985bb69b2ebd8c431c5e1d9685da0fdd96f4c2e1
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-08-23 15:54:49 +0300

    Add func. "is_even" to scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -10,3 +10,10 @@ def choose_random_number():
     random_number = randint(begin_of_range, end_of_range)
 
     return random_number
+
+
+def is_even(number):
+    """The function checks whether the number is even."""
+    divider = 2
+
+    return number % divider == 0
