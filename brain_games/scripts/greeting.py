7ce76017e798e5654689accfc45f2444f6290e9c
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-08-24 11:44:58 +0300

    Add func. "ask_user_name" to scripts/greeting.py

--- brain_games/scripts/greeting.py
+++ brain_games/scripts/greeting.py
@@ -1,4 +1,14 @@
+import prompt
+
+
 def show_greeting():
     """The function prints a welcome message."""
 
     print('Welcome to the Brain Games!')
+
+
+def ask_user_name():
+    """The function asks for the user's name and returns it."""
+
+    name = prompt.string('May I have your name? ')
+    return name
