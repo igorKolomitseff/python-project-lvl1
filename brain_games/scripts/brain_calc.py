5f81730d7b9992df66fddb5877057f97e928f841
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-08-29 18:44:07 +0300

    Add func. "ask_and_answer" to brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,6 +1,6 @@
 from random import randint
 from random import choice
-
+import prompt
 
 def choose_random_number():
     """Return an integer number from a range of numbers."""
@@ -25,3 +25,17 @@ def show_rules_game():
     """Show the rules of the game."""
 
     print('What is the result of the expression?.')
+
+
+def ask_and_answer():
+    """Select random operands and a random operator, print the question 
+    and ask the user for the answer. Return random operands, the random
+    operator and the answer."""
+    first_operand = choose_random_number()
+    second_operand = choose_random_number()
+    math_operator = choose_math_operator()
+
+    print(f'Question: {first_operand} {math_operator} {second_operand}')
+    answer = prompt.string('Your answer: ')
+
+    return (first_operand, second_operand, math_operator, answer)
