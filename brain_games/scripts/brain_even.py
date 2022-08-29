33a3134e0aca3924ab067bdfc2e5998eb026659b
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-08-29 12:31:32 +0300

    Update docstrings in scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -7,8 +7,7 @@ from brain_games.scripts.greeting import welcome_user
 
 
 def choose_random_number():
-    """The function returns an integer number from a range
-    of numbers."""
+    """Return an integer number from a range of numbers."""
     begin_of_range = 1
     end_of_range = 100
 
@@ -18,21 +17,21 @@ def choose_random_number():
 
 
 def is_even(number):
-    """The function checks whether the number is even."""
+    """Check whether the number is even."""
     divider = 2
 
     return number % divider == 0
 
 
 def show_rules_game():
-    """The function shows the rules of the game."""
+    """Show the rules of the game."""
 
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
 
 def ask_and_answer():
-    """The function selects a random number, prints the question and asks
-    the user for the answer. Returns the random number and the
+    """Select a random number, print the question and ask
+    the user for the answer. Return the random number and the
     answer."""
     random_number = choose_random_number()
 
@@ -43,7 +42,7 @@ def ask_and_answer():
 
 
 def show_correct_answer(answer, correct_answer, user_name):
-    """The function shows the correct answer"""
+    """Print the correct answer."""
 
     print(f"'{answer}' is wrong answer ;(.", end=" ")
     print(f"Correct answer was '{correct_answer}'.")
@@ -51,7 +50,7 @@ def show_correct_answer(answer, correct_answer, user_name):
 
 
 def is_even_game(user_name):
-    """The function asks the user if the number is even."""
+    """Ask the user if the number is even."""
     count_correct_answers = 0  # counter of correct answers
 
     random_number, answer = ask_and_answer()
