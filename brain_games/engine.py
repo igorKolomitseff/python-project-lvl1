f26993b166452c923add7443d023d9db8327a504
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-08-31 13:15:22 +0300

    Add func. "launch_game" to brain_games/engine.py
    Added general logic from files: scripts/brain_even and scripts/brain_calc,
    namely: greeting the user, printing the description of the game, printing
    the question, getting and comparing the correct answer and the user's answer.

---
+++ brain_games/engine.py
@@ -0,0 +1,35 @@
+import prompt
+
+
+NUMBERS_OF_ROUNDS = 3
+
+
+def launch_game(game):
+    """Run a game."""
+
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    
+    print(game.DESCRIPTION_OF_GAME)
+
+    count_correct_answers = 0
+
+    while count_correct_answers < NUMBERS_OF_ROUNDS:
+        question, correct_answer = game.get_question_answer()
+
+        print(question)
+        user_answer = prompt.string('Your answer: ')
+
+        if user_answer == correct_answer:
+            count_correct_answers += 1
+
+            print(('Correct!'))
+
+        else:
+            print(f"'{user_answer}' is wrong answer ;(.", end=" ")
+            print(f"Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {user_name}!")
+            return  # exit from the body of the loop and from the function
+
+    print(f'Congratulations, {user_name}!')
