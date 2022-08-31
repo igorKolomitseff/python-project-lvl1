1df4bc8dd62f7dfa36a946a1f84290d741f56d03
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-08-31 16:21:33 +0300

    Fix brain_games/engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -10,7 +10,7 @@ def launch_game(game):
     print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!')
-    
+
     print(game.DESCRIPTION_OF_GAME)
 
     count_correct_answers = 0
