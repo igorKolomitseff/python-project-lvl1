6a6a598a1ee6c8e8411530daa545343197bb4a45
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-08-31 13:55:25 +0300

    Add functions for launch of the brain_even.py
    The added functions help to run the script brain_even.py with
    the general logic that is contained in engine.py.

---
+++ brain_games/games/even.py
@@ -0,0 +1,26 @@
+from random import randint
+
+DESCRIPTION_OF_GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_even(number):
+    """Check whether the number is even."""
+    divider = 2
+
+    return number % divider == 0
+
+
+def get_question_answer():
+    """Get a random number as a question and the correct answer."""
+    begin_of_range = 1
+    end_of_range = 100
+
+    random_number = randint(begin_of_range, end_of_range)
+    question = f'Question: {random_number}'
+
+    if is_even(random_number):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    
+    return (question, correct_answer)
