42b3aab376bb9ced804d062ff1920a2c034c5de2
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-09-05 11:35:54 +0300

    Fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -10,7 +10,7 @@ def is_prime(number):
     if number < 2:
         return False
 
-    for i in range(2, (number / 2) + 1):
+    for i in range(2, round(number / 2)):
         if number % i == 0:
             return False
 
