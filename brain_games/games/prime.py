5aee3e0c8d770aa6c1fbfc75e2fe47431f8e2a8b
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-09-05 11:24:26 +0300

    Fix linter errors in prime.py and brain_prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,6 +1,7 @@
 from random import randint
 
-GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+GAME_DESCRIPTION = 'Answer "yes" if given number is prime. ' \
+                   'Otherwise answer "no".'
 
 
 def is_prime(number):
@@ -8,13 +9,13 @@ def is_prime(number):
 
     if number < 2:
         return False
-    
+
     for i in range(2, (number / 2) + 1):
         if number % i == 0:
             return False
-    
+
     return True
-        
+
 
 def get_question_answer():
     """Get a random number as a question and the correct answer."""
