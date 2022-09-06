e9133bdd08777c1dc139c5ce8133a4754deb8bc7
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2022-09-06 14:29:03 +0300

    Refactor progression.py
    Changed the logic of selecting a random number from the sequence and
    replacing this number with "..".

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,4 +1,4 @@
-from random import randint, choice
+from random import randint
 
 
 GAME_DESCRIPTION = 'What number is missing in the progression?.'
@@ -11,19 +11,23 @@ def get_question_answer():
     end_of_range = 100
     min_step = 1
     max_step = 10
+    min_index_item = 0
+    max_index_item = 9
 
-    random_number = randint(begin_of_range, end_of_range)
+    first_number = randint(begin_of_range, end_of_range)
     random_step = randint(min_step, max_step)
-    progression = [f'{random_number}']
+    progression = [f'{first_number}']
 
     for i in range(1, PROGRESSION_LENGTH):
         progression.append(str(int(progression[i - 1]) + random_step))
-
-    miss_number = f'{choice(progression)}'
+    
+    index_miss_number = randint(min_index_item, max_index_item)
+    miss_number = progression[index_miss_number]
+    progression[index_miss_number] = '..'
 
     progression = ' '.join(progression)
 
-    question = f'Question: {progression.replace(miss_number, "..")}'
+    question = f'Question: {progression}'
     correct_answer = miss_number
 
     return (question, correct_answer)
